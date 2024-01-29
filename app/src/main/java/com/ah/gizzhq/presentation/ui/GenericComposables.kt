febc0eaa0720f93b543553a775e83c743a62a7dd
Alex Hosszu <ahosszu@airtouchmedia.com>
2024-01-29 11:33:08 +0200

    created generic compose elements

---
+++ app/src/main/java/com/ah/gizzhq/presentation/ui/GenericComposables.kt
@@ -0,0 +1,72 @@
+package com.ah.gizzhq.presentation.ui
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import com.ah.gizzhq.R
+import com.ah.gizzhq.presentation.ui.register.RegisterUiEvent
+
+@Composable
+fun PasswordTextField(label: String, valuePassword: String, onValueChange: (String) -> Unit, isPasswordValid: Boolean) {
+    var isVisible by remember { mutableStateOf(false) }
+
+    TextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = valuePassword,
+        onValueChange = onValueChange,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        visualTransformation = if (isVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        enabled = true,
+        singleLine = true,
+        label = { Text(text = label) },
+        isError = !isPasswordValid,
+        trailingIcon = {
+            IconButton(onClick = {
+                isVisible = !isVisible
+            }) {
+                Icon(
+                    painterResource(id = if (isVisible) R.drawable.ic_visibility_on else R.drawable.ic_visibility_off),
+                    contentDescription = "Visibility",
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun EmailTextField(label: String, valueEmail: String, onValueChange: (String) -> Unit, isEmailValid: Boolean) {
+    TextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = valueEmail,
+        onValueChange = onValueChange,
+        enabled = true,
+        singleLine = true,
+        label = { Text(text = label) },
+        isError = !isEmailValid
+    )
+}
+
+@Composable
+fun ErrorText(text: String) {
+    Text(
+        modifier = Modifier.fillMaxWidth(),
+        text = text,
+        textAlign = TextAlign.Start,
+        color = Color.Red
+    )
+}
--- app/src/main/java/com/ah/gizzhq/presentation/ui/register/RegisterScreen.kt
+++ app/src/main/java/com/ah/gizzhq/presentation/ui/register/RegisterScreen.kt
@@ -6,37 +6,25 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.ah.gizzhq.R
 import com.ah.gizzhq.presentation.theme.GizzHQTheme
+import com.ah.gizzhq.presentation.ui.EmailTextField
+import com.ah.gizzhq.presentation.ui.ErrorText
+import com.ah.gizzhq.presentation.ui.PasswordTextField
 
 @Composable
 fun RegisterRoute(
@@ -54,7 +42,7 @@ fun RegisterRoute(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-internal fun RegisterScreen (
+internal fun RegisterScreen(
     email: String,
     password: String,
     retypedPassword: String,
@@ -86,47 +74,38 @@ internal fun RegisterScreen (
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        TextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = email,
+
+        EmailTextField(label = "Email",
+            valueEmail = email,
             onValueChange = { email ->
-                onEvent(RegisterUiEvent.EmailChanged(email))
-            },
-            enabled = true,
-            label = { Text(text = "Email") },
-            isError = !uiState.isEmailValid
-        )
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = emailError,
-            textAlign = TextAlign.Start,
-            color = Color.Red
-        )
+            onEvent(RegisterUiEvent.EmailChanged(email))
+        }, isEmailValid = uiState.isEmailValid)
+        ErrorText(text = emailError)
+
         Spacer(modifier = Modifier.height(16.dp))
-        PasswordTextField(label = "Password",
+        PasswordTextField(
+            label = "Password",
             valuePassword = password,
             onValueChange = { password -> onEvent(RegisterUiEvent.PasswordChanged(password)) },
-            isValid = uiState.isPasswordValid
+            isPasswordValid = uiState.isPasswordValid
         )
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = passwordError,
-            textAlign = TextAlign.Start,
-            color = Color.Red
-        )
+        ErrorText(text = passwordError)
+
         Spacer(modifier = Modifier.height(16.dp))
-        PasswordTextField(label = "Retyped Password",
+        PasswordTextField(
+            label = "Retyped Password",
             valuePassword = retypedPassword,
-            onValueChange = { retypedPassword -> onEvent(RegisterUiEvent.RetypedPasswordChanged(retypedPassword)) },
-            isValid = uiState.isRetypedPasswordValid
+            onValueChange = { retypedPassword ->
+                onEvent(
+                    RegisterUiEvent.RetypedPasswordChanged(
+                        retypedPassword
+                    )
+                )
+            },
+            isPasswordValid = uiState.isRetypedPasswordValid
         )
 
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = retypedPasswordError,
-            textAlign = TextAlign.Start,
-            color = Color.Red
-        )
+        ErrorText(text = retypedPasswordError)
         Spacer(modifier = Modifier.height(36.dp))
         Button(
             onClick = {
@@ -138,33 +117,6 @@ internal fun RegisterScreen (
             Text(text = "Register Account")
         }
     }
-}
-
-@Composable
-private fun PasswordTextField(label: String, valuePassword: String, onValueChange: (text: String) -> Unit, isValid: Boolean) {
-    var isVisible by remember { mutableStateOf(false) }
-
-    TextField(
-        modifier = Modifier.fillMaxWidth(),
-        value = valuePassword,
-        onValueChange = onValueChange,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-        visualTransformation = if (isVisible) VisualTransformation.None else PasswordVisualTransformation(),
-        enabled = true,
-        singleLine = true,
-        label = { Text(text = label) },
-        isError = !isValid,
-        trailingIcon = {
-            IconButton(onClick = {
-                isVisible = !isVisible
-            }) {
-                Icon(
-                    painterResource(id = if (isVisible) R.drawable.ic_visibility_on else R.drawable.ic_visibility_off),
-                    contentDescription = "Visibility",
-                )
-            }
-        }
-    )
 }
 
 @Preview(showBackground = true)
