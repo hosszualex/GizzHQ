7df7fcc62da6362773e515ce7dd5793917526c72
Alex Hosszu <ahosszu@airtouchmedia.com>
2024-02-25 22:40:05 +0200

    added bottom sheet functionality

--- app/src/main/java/com/ah/gizzhq/domain/utils/Extensions.kt
+++ app/src/main/java/com/ah/gizzhq/domain/utils/Extensions.kt
@@ -1,2 +1,28 @@
 package com.ah.gizzhq.domain.utils
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+
+
+fun defaultLog(message: String) {
+    Log.d("=======>", message)
+}
+
+fun Uri.toBitmap(context: Context): Bitmap? {
+    val bitmap: Bitmap? = if (Build.VERSION.SDK_INT < 28) {
+        MediaStore.Images
+            .Media.getBitmap(context.contentResolver, this)
+
+    } else {
+        val source = ImageDecoder
+            .createSource(context.contentResolver, this)
+        ImageDecoder.decodeBitmap(source)
+    }
+
+    return bitmap
+}
--- app/src/main/java/com/ah/gizzhq/presentation/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/ah/gizzhq/presentation/ui/profile/ProfileScreen.kt
@@ -1,5 +1,8 @@
 package com.ah.gizzhq.presentation.ui.profile
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
@@ -7,10 +10,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.rememberScrollState
@@ -20,25 +27,38 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
 import com.ah.gizzhq.R
+import com.ah.gizzhq.domain.utils.defaultLog
+import com.ah.gizzhq.domain.utils.toBitmap
 import com.ah.gizzhq.presentation.theme.GizzHQTheme
 import com.ah.gizzhq.presentation.ui.ProfileViewModel
+import kotlinx.coroutines.launch
 
 
 @Preview(showBackground = true)
@@ -49,24 +69,47 @@ fun ProfilePreview() {
     }
 }
 
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(viewModel: ProfileViewModel = hiltViewModel()) {
+    var isSheetOpen by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    val context = LocalContext.current
+    val defaultImageAvatar =
+        ContextCompat.getDrawable(context, R.drawable.ic_profile)?.toBitmap()!!.asImageBitmap()
+    var bitmap by remember {
+        mutableStateOf<ImageBitmap>(defaultImageAvatar)
+    }
+
+    val defaultProfileAvatars = remember {
+        mutableStateListOf<Int>(
+            R.drawable.ic_profile,
+            R.drawable.avatar_1,
+            R.drawable.avatar_2,
+            R.drawable.avatar_3
+        )
+    }
+
+    val importedProfileAvatars = remember {
+        mutableStateListOf<Uri>()
+    }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        uri?.let {
+            importedProfileAvatars.add(it)
+        }
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
             .padding(8.dp)
     ) {
-        var isSheetOpen by rememberSaveable {
-            mutableStateOf(false)
-        }
-
-        val imageUri = rememberSaveable { mutableStateOf("") }
-        val painter = rememberAsyncImagePainter(
-            imageUri.value.ifBlank { R.drawable.ic_profile }
-        )
 
         Column(
             modifier = Modifier
@@ -74,26 +117,52 @@ fun ProfileScreen(viewModel: ProfileViewModel = hiltViewModel()) {
                 .padding(8.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            ImageAvatar(R.drawable.ic_profile) {
-                isSheetOpen = true
-            }
+            Image(
+                bitmap = bitmap,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(CircleShape)
+                    .border(
+                        BorderStroke(4.dp, MaterialTheme.colorScheme.secondary),
+                        CircleShape
+                    )
+                    .padding(4.dp)
+                    .clickable {
+                        isSheetOpen = true
+                    },
+                contentScale = ContentScale.Crop,
+                colorFilter = if (bitmap == defaultImageAvatar) // todo alex: find out why this changes color on first tap
+                    ColorFilter.tint(color = MaterialTheme.colorScheme.secondary)
+                else
+                    null
+            )
+
             Text(text = "Change profile picture")
+
+            Spacer(modifier = Modifier.height(24.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 4.dp, end = 4.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "Display Name:", modifier = Modifier.width(110.dp))
+                TextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = "",
+                    onValueChange = { },
+                    enabled = true,
+                    singleLine = true,
+                )
+            }
+
         }
 
-        val sheetState = rememberModalBottomSheetState()
-
-        //viewModel.firebaseStorage()
-
+        val bottomSheetState = rememberModalBottomSheetState()
         if (isSheetOpen) {
-            val profileAvatars = listOf(
-                R.drawable.ic_profile,
-                R.drawable.avatar_1,
-                R.drawable.avatar_2,
-                R.drawable.avatar_3,
-                R.drawable.ic_add
-            )
             ModalBottomSheet(
-                sheetState = sheetState,
+                sheetState = bottomSheetState,
                 onDismissRequest = {
                     isSheetOpen = false
                 }) {
@@ -103,9 +172,24 @@ fun ProfileScreen(viewModel: ProfileViewModel = hiltViewModel()) {
                     verticalArrangement = Arrangement.spacedBy(10.dp),
                     horizontalArrangement = Arrangement.spacedBy(10.dp)
                 ) {
-                    items(profileAvatars.size) { item ->
-                        ImageAvatar(avatar = profileAvatars[item]) {
-                            // todo: on click, select this as new avatar
+                    items(defaultProfileAvatars.size) { item ->
+                        ImageAvatar(avatar = defaultProfileAvatars[item]) {
+                            bitmap = ContextCompat.getDrawable(context, defaultProfileAvatars[item])
+                                ?.toBitmap()!!.asImageBitmap()
+                            isSheetOpen = false
+                        }
+                    }
+                    items(importedProfileAvatars.size) { item ->
+                        ImportedImageAvatar(avatar = importedProfileAvatars[item]) {
+                            bitmap =
+                                importedProfileAvatars[item].toBitmap(context)!!.asImageBitmap()
+                            isSheetOpen = false
+                        }
+                    }
+                    item {
+                        ImageAvatar(avatar = R.drawable.ic_add) {
+                            launcher.launch("image/*")
+                            defaultLog("Clicked on add")
                         }
                     }
                 }
@@ -130,6 +214,27 @@ fun ImageAvatar(avatar: Int, onAvatarClicked: (() -> Unit)? = null) {
                 onAvatarClicked?.invoke()
             },
         contentScale = ContentScale.Crop,
-        colorFilter = if (avatar == R.drawable.ic_profile || avatar == R.drawable.ic_add) ColorFilter.tint(color = MaterialTheme.colorScheme.secondary) else null
+        colorFilter = if (avatar == R.drawable.ic_profile || avatar == R.drawable.ic_add) ColorFilter.tint(
+            color = MaterialTheme.colorScheme.secondary
+        ) else null
+    )
+}
+
+@Composable
+fun ImportedImageAvatar(avatar: Uri, onAvatarClicked: (() -> Unit)? = null) {
+    Image(
+        painter = rememberAsyncImagePainter(avatar),
+        contentDescription = null, modifier = Modifier
+            .size(120.dp)
+            .clip(CircleShape)
+            .border(
+                BorderStroke(4.dp, MaterialTheme.colorScheme.secondary),
+                CircleShape
+            )
+            .padding(4.dp)
+            .clickable {
+                onAvatarClicked?.invoke()
+            },
+        contentScale = ContentScale.Crop
     )
 }
